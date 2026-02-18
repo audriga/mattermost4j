f022c6d13059e20c5eb14d79f5108407273acdb5
maximilian <maxmilian@audriga.com>
2026-02-18 18:22:43 +0100

    delete: removed module ImportList

--- mattermost4j-core/src/main/java/net/bis5/mattermost/client4/api/ImportApi.java
+++ mattermost4j-core/src/main/java/net/bis5/mattermost/client4/api/ImportApi.java
@@ -1,8 +1,6 @@
 package net.bis5.mattermost.client4.api;
 
 import net.bis5.mattermost.client4.ApiResponse;
-import net.bis5.mattermost.model.ImportList;
-import java.io.IOException;
 import java.util.List;
 
 /**
